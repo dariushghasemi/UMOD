52202ea7da41e819496e5ee3f89acb3c1a4cf755
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-08-19 19:33:04 +0200

    try different models for Cristian's SNP; INT makes dosage significant
    add PCs; spearman correlation

--- scripts/umod_chris.R
+++ scripts/umod_chris.R
@@ -4,21 +4,27 @@
 
 
 # inputs
-path_umod_chris <- "/scratch/dariush.ghasemi/projects/UMOD/data/chr16_20381010_rs77924615.txt"
+path_umod_chris <- "/scratch/dariush.ghasemi/projects/UMOD/data/chr16-20348509-rs13335818.txt" #chr16_20381010_rs77924615.txt
 path_clinicals <- "~/projects/shroom3/data/chris_q-norm.csv"
-
+path_pcs <- "~/projects/shroom3/data/CHRIS13K.GT.evecs"
 
 # outputs
+
+#----------------------#
+# inverse-normal transformation
+do_INT = function(values) {
+  qnorm((rank(values, na.last = "keep", ties.method = "random") - 0.5) / sum(!is.na(values)))
+}
 
 
 #----------------------#
 # read dosage of UMOD in CHRIS
 headers <- c("AID", "ID", "DS")
-umod_dosage <- read.delim(path_umod_chris, sep = "\t", col.names = headers, stringsAsFactors = FALSE)
+umod_dosage <- read.delim(path_umod_chris, sep = "\t", stringsAsFactors = FALSE) #col.names = headers, 
 
 # load chris phenotypes
 chris <- read.csv(path_clinicals, stringsAsFactors = FALSE)
-
+pcs <- read.delim(path_pcs, stringsAsFactors = FALSE)
 
 #----------------------#
 # prepare data for model/visualization
@@ -32,10 +38,12 @@ umod_combined <- chris %>%
     umod_dosage %>% dplyr::select(AID, DS), # merge phenotype with genotype 
     join_by(AID)
     ) %>%
+  dplyr::rename(dosage = DS) %>%
+  left_join(pcs, join_by(AID == X.IND_ID)) %>%
   # categorize numeric variables for model/visualization
   dplyr::mutate(
     ds_cat  = cut(
-      DS,
+      dosage,
       breaks=c(-Inf, 0.500, 1.500, Inf),
       labels=c("0", "1", "2")),
     age_cat = cut(
@@ -50,17 +58,20 @@ umod_combined <- chris %>%
 
 #tab <- as.matrix(prop.table(table(UMOD$DS_Level, UMOD$Age_cat), margin = 2))
 
+res_cor <- cor.test(umod_combined$Age, umod_combined$dosage, method = "spearman")
+
 
 #----------------------#
 # plots
 
 # age vs. SNP
 umod_combined %>%
-  ggplot(aes(age_cat, DS)) +
-  geom_violin(aes(color = age_cat), trim = FALSE, alpha=0.9) +
+  ggplot(aes(age_cat, dosage)) +
+  geom_violin(aes(fill = age_cat), trim = FALSE, alpha=0.9) +
   labs(
     x = "Categorized age of CHRIS participants",
     y = "Dosage of UMOD lead SNP\n(rs13335818)",
+    #y = "CHRIS lead SNP dosage\n(rs77924615)",
     fill = "Categorized\nAge"
        ) +
   theme_bw() +
@@ -74,9 +85,19 @@ umod_combined %>%
 ggsave(filename = "19-Aug-25_violin_plot_chris_umod_vs_age.png", height = 5.5, width = 9, dpi = 150)
 
 #----------------------#
-# linear model
+# linear model 
 
-umod_model <- lm(eGFRw.log.Res ~ DS, data = umod_combined)
+# different models
+base_model <- "do_INT(eGFRw.log.Res) ~ dosage"
+inclusive_model <- "do_INT(eGFRw.log.Res) ~ dosage + Age"
+interaction_model <- "do_INT(eGFRw.log.Res) ~ dosage * Age"
+
+# to include other covariates
+#+ Age + Sex + PC1 + PC2 + PC3 + PC4 + PC5
+
+# fit models
+umod_model <- lm(interaction_model, data = umod_combined)
+
 
 # illustrate the model summary
 broom::tidy(umod_model) %>%
