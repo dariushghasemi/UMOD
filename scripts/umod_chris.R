8b58fbc749a3f34e10d39f6fa2c844c502c97d60
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-08-19 15:59:09 +0200

    clean initial R script; rum linear model

--- scripts/umod_chris.R
+++ scripts/umod_chris.R
@@ -3,25 +3,82 @@
 #=========================================#
 
 
-#-----------------------------------------------------#
-#---------------------- UMOD -------------------------
-#-----------------------------------------------------#
-UMODvcf <- read.delim("D:\\Dariush\\PhD\\Analysis\\Data\\chr16-20348509-rs13335818.txt", 
-                      sep = "\t", stringsAsFactors = FALSE)
+# inputs
+path_umod_chris <- "/scratch/dariush.ghasemi/projects/UMOD/data/chr16-20348509-rs13335818.txt"
+path_clinicals <- "~/projects/shroom3/data/chris_q-norm.csv"
 
-UMOD <-  merge( chris[c("AID","Sex","Age","eGFRw.log.Res","eGFR","UACR")],
-                UMODvcf[c("AID", "DS")], 
-                by="AID", all=FALSE)
+# outputs
 
-UMOD <- UMOD %>% 
-  mutate(DS_Level = cut(DS, breaks=c(-Inf, 0.500, 1.500, Inf), labels=c("0", "1", "2"))) %>% 
-  mutate(Age_cat = cut(Age, breaks=c(18, 30, 40, 50, 60, 70, 94),
-                       labels=c("18-30","30-40","40-50","50-60","60-70","70-94"))) #%>%
+
+#----------------------#
+# read dosage of UMOD in CHRIS
+umod_dosage <- read.delim(path_umod_chris, sep = "\t", stringsAsFactors = FALSE)
+
+# load chris phenotypes
+chris <- read.csv(path_clinicals, stringsAsFactors = FALSE)
+
+
+#----------------------#
+# prepare data for model/visualization
+umod_combined <- chris %>%
+  dplyr::select(
+    AID, Sex, Age, UACR, eGFRw.log.Res, eGFR
+    ) %>%
+  dplyr::filter(!is.na(eGFRw.log.Res)) %>% # remove rows with missing phenotype
+  dplyr::mutate(across(Age, as.numeric)) %>% # convert age to numeric
+  inner_join(
+    umod_dosage %>% dplyr::select(AID, DS), # merge phenotype with genotype 
+    join_by(AID)
+    ) %>%
+  # categorize numeric variables for model/visualization
+  dplyr::mutate(
+    ds_cat  = cut(
+      DS,
+      breaks=c(-Inf, 0.500, 1.500, Inf),
+      labels=c("0", "1", "2")),
+    age_cat = cut(
+      Age, 
+      breaks=c(18, 30, 40, 50, 60, 70, 94),
+      labels=c("18-30","30-40","40-50","50-60","60-70","70-94"))
+  )
+
+
 #group_by(Age_cat) %>%
 #summarise(n(), M = mean(DS), SD = sd(DS))
-ggplot(aes(Age_cat, DS)) +
-  geom_violin(aes(fill = Age_cat), trim = FALSE, alpha=0.9) + theme_bw()
 
-tab <- as.matrix(prop.table(table(UMOD$DS_Level, UMOD$Age_cat), margin = 2))
+#tab <- as.matrix(prop.table(table(UMOD$DS_Level, UMOD$Age_cat), margin = 2))
+
+
+#----------------------#
+# plots
+
+# age vs. SNP
+umod_combined %>%
+  ggplot(aes(age_cat, DS)) +
+  geom_violin(aes(color = age_cat), trim = FALSE, alpha=0.9) +
+  labs(
+    x = "Categorized age of CHRIS participants",
+    y = "Dosage of UMOD lead SNP\n(rs13335818)",
+    fill = "Categorized\nAge"
+       ) +
+  theme_bw() +
+  theme(
+    axis.text = element_text(size = 12),
+    axis.ticks.length = unit(5, "points"),
+    axis.title = element_text(size = 12, face = 2)
+  )
+
+
+ggsave(filename = "19-Aug-25_violin_plot_chris_umod_vs_age.png", height = 5.5, width = 9, dpi = 150)
+
+#----------------------#
+# linear model
+
+umod_model <- lm(eGFRw.log.Res ~ DS, data = umod_combined)
+
+# illustrate the model summary
+broom::tidy(umod_model) %>%
+  dplyr::mutate(across(c(estimate:statistic), ~round(.x, 3))) %>%
+  DT::datatable()
 
 
