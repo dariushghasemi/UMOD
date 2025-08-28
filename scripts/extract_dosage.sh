a1e5f431d09b2971daf8b0deb1010b34fdb20a53
dariushghasemi <ghasemi.dariush@yahoo.com>
2025-08-29 00:22:03 +0200

    extract dosages from variants list

--- scripts/extract_dosage.sh
+++ scripts/extract_dosage.sh
@@ -12,18 +12,20 @@
 genotype=/scratch/dariush.ghasemi/projects/chr16.dose.vcf.gz
 # initial VCF was: /scratch/ekoenig/CHRIS_CORRECTED/10K/Imputed/TOPMedR2/20210409/chr16.vcf.gz
 
-# path to output
-ofile=/scratch/dariush.ghasemi/projects/UMOD/data/chr16_20381010_rs77924615.txt
-
+# variants file
+snps=/scratch/dariush.ghasemi/projects/UMOD/data/umod_hg38.snps
 # coordinates of a geneomic region (also position of interested SNP)
-region=chr16:20381010   # hg38 for UMOD hit in CHRIS (rs77924615)
-#region can also be defined as chr16_20381010-20381010
+# -r: region can also be defined as chr16_20381010-20381010
+
+# path to output
+ofile=/scratch/dariush.ghasemi/projects/UMOD/data/umod_hg38.dosage
+
 
 # load libraries on server
 source /exchange/healthds/singularity_functions
 
 # Extracting dosage level of UMOD lead SNP in CHRIS
-bcftools query -f '[%SAMPLE\t%ID\t%DS\n]' $genotype -r $region -o $ofile
+bcftools query -f '[%SAMPLE\t%ID\t%DS\n]' $genotype -R $snps -o $ofile
 
 # for full characteristics of the variant, use below term for the query
 #-f '[%SAMPLE\t%CHROM\t%POS\t%ID\t%REF\t%ALT\t%QUAL\t%FILTER\t%GT\t%DS\t%HDS\t%GP\n]'
